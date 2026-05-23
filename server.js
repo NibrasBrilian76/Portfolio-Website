ad67817c59335682a5a5b7b647de0a8e92bf3b4f
NibrasBrilian76 <chokit404@gamil.com>
2026-05-23 18:34:57 +0700

    increase payload limit

--- server.js
+++ server.js
@@ -18,8 +18,10 @@ const MySQLStore = require("express-mysql-session")(session);
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: "10mb" }));
 app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 
 const db = mysql.createConnection({
